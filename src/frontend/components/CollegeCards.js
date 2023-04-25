6ba51188936d955fabbd2032fc78b35b5bb896e7
PadNim14 <PadNim14@github.com>
2023-04-25 15:51:42 -0400

    cleaned up comments in CollegeCards.js

--- src/frontend/components/CollegeCards.js
+++ src/frontend/components/CollegeCards.js
@@ -1,48 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// function SearchResults(event, collegeName) {
-//   event.preventDefault();
-//   const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(collegeName)}`;
-//   window.open(searchUrl, "_blank");
-// }
-
-// function CollegeCards({ colleges }) {
-//   return (
-//     <div className='college-cards'>
-//       {colleges.map((college) => (
-//         <Link to={`/search?q=${encodeURIComponent(college)}`} onClick={(event) => SearchResults(event, college)} style={{color: 'white'}}>
-//           <CollegeCard key={college} name={college} style='college-name' />
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
-
-// function CollegeCard({ name }) {
-//   return <div className='college-card'>{name}</div>;
-// }
-
-// export default CollegeCards;
-
-// function SearchResults(event, url) {
-//   event.preventDefault();
-//   window.open(url, "_blank");
-// }
-
-// function CollegeCards({ colleges }) {
-//   return (
-//     <div className='college-cards'>
-//       {colleges.map((college) => (
-//         <div key={college[0]} className='college-card'>
-//           <button className='btn btn-primary' onClick={(event) => SearchResults(event, college[1])}>
-//             {college[0]}
-//           </button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
 function SearchResults(event, url) {
   event.preventDefault();
   const baseUrl = "http://"; // replace with the base URL of your website
@@ -64,22 +19,6 @@ function CollegeCards({ colleges }) {
   );
 }
 
-// function CollegeCards({ colleges }) {
-//   return (
-//     <div className='college-cards'>
-//       {colleges.map((college) => (
-//         <div key={college[0]} className='college-card'>
-//           <a href={college[1]} target='_blank' rel='noopener noreferrer' className='btn btn-primary'>
-//             {college[0]}
-//           </a>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-
-// export default CollegeCards;
 
 
 export default CollegeCards;
