2f8f6ad39df39ad1efb19040be7901b7611ccbcc
WeatherMan <37720981+ConnorWeatherly@users.noreply.github.com>
2023-04-30 23:14:18 -0400

    Update firebase.py

--- src/backend/firebase.py
+++ src/backend/firebase.py
@@ -1,13 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 
 cred = credentials.Certificate("unirecs-787f9-firebase-adminsdk-xuw4v-c1ea8e1dfd.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-quiz_res = db.collection(u'quizResponses')
-docs = quiz_res.get()
+def get_user_data(user_id):
+    query = db.collection('quizResponses').where(filter=FieldFilter("userId", "==", user_id))
+    return query.get()
 
 if __name__ == "__main__":
+    quiz_res = db.collection(u'quizResponses')
+    docs = quiz_res.get()[0]
+
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
