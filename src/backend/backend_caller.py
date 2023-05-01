3d6c28ace8d222008729f3cffdc83fee068966cc
WeatherMan <37720981+ConnorWeatherly@users.noreply.github.com>
2023-04-30 23:19:30 -0400

    Update and rename backend_test.py to backend_caller.py
    Master function to call with user ID

--- src/backend/backend_caller.py
+++ src/backend/backend_caller.py
@@ -1,10 +1,16 @@
 from firebase import get_user_data
 from similarity_functions import make_user_recs
 
-if __name__ == "__main__":
-    user_id = "Ilp1CPwB7nNN0MsAparujOmXYeE3"
+def make_rec(user_id):
     user = get_user_data(user_id)
     user_data = [question.to_dict() for question in user]
     recs = make_user_recs(user_data)
+
+    return recs
+
+if __name__ == "__main__":
+    user_id = "Ilp1CPwB7nNN0MsAparujOmXYeE3"
+    recs = make_rec(user_id)
     for idx, rec in enumerate(recs[:25]):
         print(f"{idx+1})\t{rec}")
+
