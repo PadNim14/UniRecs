200fc320da224f4d24dc07d7516ff4eac923db19
WeatherMan <37720981+ConnorWeatherly@users.noreply.github.com>
2023-04-30 23:24:10 -0400

    Update firebase.py
    Updated the Filter functionality to only include if present

--- src/backend/firebase.py
+++ src/backend/firebase.py
@@ -1,13 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from google.cloud.firestore_v1.base_query import FieldFilter
 
 cred = credentials.Certificate("unirecs-787f9-firebase-adminsdk-xuw4v-c1ea8e1dfd.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def get_user_data(user_id):
-    query = db.collection('quizResponses').where(filter=FieldFilter("userId", "==", user_id))
+    try:
+        from google.cloud.firestore_v1.base_query import FieldFilter
+        query = db.collection('quizResponses').where(filter=FieldFilter("userId", "==", user_id))
+    except:
+        query = db.collection('quizResponses').where("userId", "==", user_id)
     return query.get()
 
 if __name__ == "__main__":
@@ -16,3 +19,4 @@ if __name__ == "__main__":
 
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
